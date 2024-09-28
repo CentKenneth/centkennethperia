e8f2271c943cacd00654f8aa6ac3fffab82f906e
CentKenneth <mycentkennethperia@gmail.com>
2024-09-28 14:48:06 +0800

    Setup GitHub Pages deployment 6

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/centkennethperia/', // Ensure this is correct.
+  base: '/centkennethperia', // Ensure this is correct.
 });
